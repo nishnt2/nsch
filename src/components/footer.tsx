1976812f0e4d0039a24a34ab85a167dfb56b37d9
nishnt2 <patilnishant222@gmail.com>
2025-06-19 16:51:53 +0530

    created scratch components dfor layout

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,8 +1,8 @@
 export default function Footer() {
   return (
     <footer className="mt-16 border-t bg-[#030f1a]/70 border-gray-300 dark:border-gray-700 text-center py-10 text-sm text-gray-600 dark:text-gray-400">
-      <div className="mb-4">
-        © {new Date().getFullYear()} Nishant Patil. All rights reserved.
+      <div className="mb-4 font-code">
+        © {new Date().getFullYear()} Nishant. Made with Nextjs.
       </div>
       <div className="flex justify-center space-x-6 text-base">
         <a
@@ -22,7 +22,7 @@ export default function Footer() {
           LinkedIn
         </a>
         <a
-          href="https://twitter.com/yourusername"
+          href="https://twitter.com/ImNishant3"
           target="_blank"
           rel="noopener noreferrer"
           className="hover:text-black dark:hover:text-white transition"

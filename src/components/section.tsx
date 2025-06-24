0af43afc2c3db79fec5a99787be21d2f13ce5ef0
nishnt2 <patilnishant222@gmail.com>
2025-06-25 00:46:27 +0530

    updated the section component

--- src/components/section.tsx
+++ src/components/section.tsx
@@ -1,10 +1,25 @@
 import { ReactNode } from 'react';
+import clsx from 'clsx'; // Optional but helpful for cleaner conditional classes
 
-export default function Section({ children }: { children: ReactNode }) {
+export default function Section({
+  children,
+  glassMorphed = true,
+  title = '',
+}: {
+  children: ReactNode;
+  glassMorphed?: boolean;
+  title?: string;
+}) {
   return (
-    <section className="w-full rounded-md bg-[#030f1a]/70 backdrop-blur-[2px] border border-white/5 shadow-md p-6 sm:p-8">
-      <h2 className="font-code bg-primary">/Whoami</h2>
-      <h2 className="font-sans">Title</h2>
+    <section
+      className={clsx(
+        'w-full  p-6 sm:p-8',
+        glassMorphed
+          ? 'bg-glassBg backdrop-blur-[2px] rounded-md border border-white/5 shadow-md'
+          : 'bg-transparent backdrop-blur-0'
+      )}
+    >
+      <h2 className="font-code">/{title} </h2>
       {children}
     </section>
   );

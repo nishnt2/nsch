5abd387dda332e5a8708e16c57835f1bb3c1d3f4
nishnt2 <patilnishant222@gmail.com>
2025-06-29 01:37:42 +0530

    api to fetch devto blogs

---
+++ src/lib/types.ts
@@ -0,0 +1,12 @@
+export type DevToPost = {
+  id: number;
+  title: string;
+  description: string;
+  slug: string;
+  body_html: string;
+  published: boolean;
+  tag_list: string[];
+  reading_time_minutes: number;
+  url: string;
+  body_markdown: string;
+};
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,17 @@
-import { clsx, type ClassValue } from "clsx";
-import { twMerge } from "tailwind-merge";
+import { clsx, type ClassValue } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { DevToPost } from './types';
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
+}
+
+export async function getAllPosts(): Promise<DevToPost[]> {
+  const res = await fetch(`https://dev.to/api/articles?username=nshnt`);
+
+  if (!res.ok) throw new Error('Failed to fetch Dev.to articles');
+
+  const posts: DevToPost[] = await res.json();
+
+  return posts;
 }
